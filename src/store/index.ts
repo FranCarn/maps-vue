962310f4b30533e6d22cdc1299fc9297c774b1b8
@FranCarn <carnevalefranco99@gmail.com>
2024-04-14 18:40:04 -0300

    places store

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,18 +1,15 @@
 import { createStore } from "vuex";
 
 // My custom modules
-import exampleModule from "./module-template";
-import { ExampleStateInterface } from "./module-template/state";
+import { PlacesState } from "./places/state";
+import places from "./places";
 
 export interface StateInterface {
-  // Define your own store structure, using submodules if needed
-  // example: ExampleStateInterface;
-  // Declared as unknown to avoid linting issue. Best to strongly type as per the line above.
-  example: ExampleStateInterface;
+  places: PlacesState;
 }
 
 export default createStore<StateInterface>({
   modules: {
-    example: exampleModule,
+    places,
   },
 });
